fb13c1ac3fe65a8c1f349d65bdcdf65f42b7f423
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-06-28 16:05:40 +0700

    Activity 11 mengedit dan menghapus data

--- app/src/main/java/com/example/fbkelasa/adapter/AdapterLihatTeman.java
+++ app/src/main/java/com/example/fbkelasa/adapter/AdapterLihatTeman.java
@@ -1,22 +1,34 @@
 package com.example.fbkelasa.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fbkelasa.MainActivity;
 import com.example.fbkelasa.R;
+import com.example.fbkelasa.TemanEdit;
 import com.example.fbkelasa.database.Teman;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class AdapterLihatTeman extends RecyclerView.Adapter<AdapterLihatTeman.ViewHolder> {
     private ArrayList<Teman> daftarTeman;
     private Context context;
+    private DatabaseReference databaseReference;
 
     public AdapterLihatTeman(ArrayList<Teman> daftarTeman, Context context) {
         this.daftarTeman = daftarTeman;
@@ -33,21 +45,62 @@ public class AdapterLihatTeman extends RecyclerView.Adapter<AdapterLihatTeman.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String nama = daftarTeman.get(position).getNama();
+        String kode, nama, telpon;
+
+        nama = daftarTeman.get(position).getNama();
+        kode = daftarTeman.get(position).getKode();
+        telpon = daftarTeman.get(position).getTelpon();
+
         holder.tvNama.setText(nama);
 
         holder.tvNama.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                //fungsi update dan delete
-                return false;
-            }
-        });
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                popupMenu.inflate(R.menu.menuteman);
 
-        holder.tvNama.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //menampilkan detail datanya
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnEdit:
+                                Bundle bundle = new Bundle();
+                                bundle.putString("Kunci1", kode);
+                                bundle.putString("Kunci2", nama);
+                                bundle.putString("Kunci3", telpon);
+
+                                Intent intent = new Intent(v.getContext(), TemanEdit.class);
+                                intent.putExtras(bundle);
+                                v.getContext().startActivity(intent);
+                                break;
+
+                            case R.id.mnHapus:
+                                AlertDialog.Builder alertDlg = new AlertDialog.Builder(v.getContext());
+                                alertDlg.setTitle("Yakin data" + nama + " akan dihapus ?");
+                                alertDlg.setMessage("Tekan 'Ya' untuk menghapus").setCancelable(false).setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        DeleteData(kode);
+                                        Toast.makeText(v.getContext(), "Data" + nama + "berhasil dihapus", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(v.getContext(), MainActivity.class);
+                                        v.getContext().startActivity(intent);
+                                    }
+                                })
+                                        .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                                AlertDialog aDlg = alertDlg.create();
+                                aDlg.show();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+                return true;
             }
         });
     }
@@ -63,6 +116,16 @@ public class AdapterLihatTeman extends RecyclerView.Adapter<AdapterLihatTeman.Vi
         ViewHolder(View v){
             super(v);
             tvNama = (TextView) v.findViewById(R.id.tv_nama);
+
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+    }
+
+    public void DeleteData(String kode)
+    {
+        if (databaseReference != null)
+        {
+            databaseReference.child("Teman").child(kode).removeValue();
         }
     }
 }
